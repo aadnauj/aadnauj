1a16384f9e1723168050260f90441139733d21fd
Juanda Harefa <juandaharefa1@gmail.com>
2026-04-02 20:24:51 +0700

    Update bagian home page

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+
+hamburger.classList.toggle("active");
+
+navMenu.classList.toggle("active");
+
+});
